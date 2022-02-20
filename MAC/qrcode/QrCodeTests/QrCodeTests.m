ab05a2f08ce7abd3337d13e2e9b103a170f220b8
Pai Peng <sipaipv6@gmail.com>
2022-02-20 22:12:30 +0800

    add encode unit test

---
+++ MAC/qrcode/QrCodeTests/QrCodeTests.m
@@ -0,0 +1,43 @@
+//
+//  QrCodeTests.m
+//  QrCodeTests
+//
+//  Created by Pai Peng on 2022/2/20.
+//
+
+#import <XCTest/XCTest.h>
+#include "encoder.h"
+
+@interface QrCodeTests : XCTestCase
+
+@end
+
+@implementation QrCodeTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testEncode {
+    scanner_t scanner;
+    scanner.c = 0;
+    scanner.verbosity = 0;
+    
+    qrc_encode(&scanner, "TEST01234");
+    
+    
+    free(scanner.d);
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
